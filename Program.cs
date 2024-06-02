2ab5d58c60e574a40daf0dd35c595adcd3a59662
Ramil Valiyev <ramil4360616@gmail.com>
2024-06-02 17:02:24 +0400

    Add Database Connection String

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
